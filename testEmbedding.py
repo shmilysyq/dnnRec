d141f9559fc9ebb1482ae747fa0306b07dc7451a
shengyaqi <shengyaqi@corp.netease.com>
2018-11-16 18:49:15 +0800

    update test

--- testEmbedding.py
+++ testEmbedding.py
@@ -2,23 +2,26 @@ import tensorflow as tf
 
 
 def test_embedding():
-    color_data = {'color': ['R', 'G', 'B', 'A']} 
+    color_data = {'color': [['R', 'G', 'B', 'A']],'age':[1]} 
 
     color_column = tf.feature_column.categorical_column_with_vocabulary_list(
         'color', ['R', 'G', 'B'], dtype=tf.string, default_value=-1
     )
-    # age_column = tf.feature_column.numeric_column('age',shape=(1,))
-    color_embeding = tf.feature_column.embedding_column(color_column, 8)
-    color_embeding_dense_tensor = tf.feature_column.input_layer(color_data, [color_embeding])
 
-    embedding = tf.reduce_mean(color_embeding_dense_tensor,1)
+    age_column = tf.feature_column.numeric_column('age')
+    color_embeding = tf.feature_column.embedding_column(color_column, 8)
+    net = tf.feature_column.input_layer(color_data, [age_column,color_embeding])
+
+    # embedding = tf.reduce_mean(color_embeding_dense_tensor,1)
     with tf.Session() as session:
         session.run(tf.global_variables_initializer())
 
         session.run(tf.tables_initializer())
 
         print('embeding' + '_' * 40)
-        print(session.run([embedding]))
+        print(session.run([net]))
+
+test_embedding()
 
 import tensorflow as tf
 import numpy as np
@@ -26,18 +29,30 @@ _column = ['hist_i','y']
 _ValueD = [[''],[0]]
 
 def load_data():
-filenames = ["/var/data/file1.txt", "/var/data/file2.txt"]
+    ds = tf.data.TextLineDataset("./data/train.csv").skip(1)
 
-dataset = tf.data.Dataset.from_tensor_slices(filenames)
+    COLUMNS = ['SepalLength', 'SepalWidth',
+           'PetalLength', 'PetalWidth',
+           'label']
+    FIELD_DEFAULTS = [[0.0], [0.0], [0.0], [0.0], [0]]
+    def _parse_line(line):
+        # Decode the line into its fields
+        fields = tf.decode_csv(line, FIELD_DEFAULTS)
 
-# Use `Dataset.flat_map()` to transform each file as a separate nested dataset,
-# and then concatenate their contents sequentially into a single "flat" dataset.
-# * Skip the first line (header row).
-# * Filter out lines beginning with "#" (comments).
-dataset = dataset.flat_map(
-    lambda filename: (
-        tf.data.TextLineDataset(filename)
-        .skip(1)
-        .filter(lambda line: tf.not_equal(tf.substr(line, 0, 1), "#"))))
+        # Pack the result into a dictionary
+        features = dict(zip(COLUMNS,fields))
 
-load_data()
+        # Separate the label from the features
+        label = features.pop('label')
+
+        return features, label
+
+    ds = ds.map(_parse_line)
+    print(ds)
+    # with tf.Session() as session:
+    #     session.run(tf.global_variables_initializer())
+
+    #     session.run(tf.tables_initializer())
+
+    #     print(session.run([ds]))
+# load_data()
